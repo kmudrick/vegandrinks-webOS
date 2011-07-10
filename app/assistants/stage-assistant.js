bd3ca3e4c8a78622d06e50f6f3facca1db099401
Kevin Mudrick <kevin@mudrick.com>
2011-07-10 15:50:17 -0400

    New version

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -1,21 +1,39 @@
-function StageAssistant() { 
-  VeganDrinks = { };
+function StageAssistant() {
+  
+  // data object, will be keyed by category
+  this.data = { 
+    beer   : { },
+    wine   : { },
+    liquor : { }
+  };
+  
+  this.parseCount = 0;
 }
 
 StageAssistant.prototype.setup = function() {
-  var start = (new Date()).getTime();
-  var request = new Ajax.Request("misc/beer.json", {
-    method: 'get',
-    evalJSON: 'true',
-    onComplete: this.parsedData.bind(this, start)
-  });
+  for (var category in this.data) {
+    var start = (new Date()).getTime();
+    var dataLocation = "data/" + category + ".json";
+    Mojo.Log.info("Loading %s data", category);
+    var request = new Ajax.Request(dataLocation, {
+      method: 'get',
+      evalJSON: 'true',
+      onComplete: this.parsedData.bind(this, start, category)
+    });
+  }
 }
 
-StageAssistant.prototype.parsedData = function(start, transport) {
+StageAssistant.prototype.parsedData = function(start, category, transport) {
   var json = transport.responseText.evalJSON(true);
   var end = (new Date()).getTime();
   var elapsed = end - start;
-  Mojo.Log.info("Loaded data in %s ms", elapsed);
-  VeganDrinks.Beer = json;
-  Mojo.Controller.stageController.swapScene("list", "Beer", VeganDrinks.Beer);
+  Mojo.Log.info("Loaded %s data in %s ms", category, elapsed);
+  this.data[category] = json;
+  if (++this.parseCount == 3) {
+    this.onComplete();
+  }
+}
+
+StageAssistant.prototype.onComplete = function() {
+  Mojo.Controller.stageController.swapScene("main", this.data);
 }
